7b79d53289c4aa65a71ba9e2c648394862ce1565
Marcinp98 <marcinpilipczuk98@gmail.com>
2024-02-02 23:02:31 +0100

    Added debugmacros.h with macros for DebugLine, DebugPoint, DebugSphere, and DebugVector

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Items/Item.h"
+#include "Slash/DebugMacros.h"
+
 
 // Sets default values
 AItem::AItem()
@@ -15,13 +17,20 @@ AItem::AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
+
+	UWorld* World = GetWorld();
+	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
 	
+	DRAW_SPHERE(Location);
+	DRAW_VECTOR(Location, Location + Forward * 100.f);
+
 }
 
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	
 }
 
