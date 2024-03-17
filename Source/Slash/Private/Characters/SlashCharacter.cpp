df434097127e8107ed8363d3e74ce96113fe58a6
Marcinp98 <marcinpilipczuk98@gmail.com>
2024-03-17 17:12:43 +0100

    Add EnemyStates, and enemy chase behaviour when sees character

--- Source/Slash/Private/Characters/SlashCharacter.cpp
+++ Source/Slash/Private/Characters/SlashCharacter.cpp
@@ -54,7 +54,8 @@ void ASlashCharacter::BeginPlay()
 			Subsystem->AddMappingContext(SlashContext, 0);
 		}
 	}
-	
+
+	Tags.Add(FName("SlashCharacter"));
 }
 
 
--- Source/Slash/Private/Enemy/Enemy.cpp
+++ Source/Slash/Private/Enemy/Enemy.cpp
@@ -148,7 +148,16 @@ AActor* AEnemy::ChoosePatrolTarget()
 
 void AEnemy::PawnSeen(APawn* SeenPawn)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Pawn Seen!"));
+	if (EnemyState == EEnemyState::EES_Chasing) return;
+	if (SeenPawn->ActorHasTag(FName("SlashCharacter")))
+	{
+		EnemyState = EEnemyState::EES_Chasing;
+		GetWorldTimerManager().ClearTimer(PatrolTimer);
+		GetCharacterMovement()->MaxWalkSpeed = 300.f;
+		CombatTarget = SeenPawn;
+		MoveToTarget(CombatTarget);
+		UE_LOG(LogTemp, Warning, TEXT("Seen Pawn, now chasing"));
+	}
 }
 
 void AEnemy::PlayHitReactMontage(const FName& SectionName)
@@ -165,8 +174,14 @@ void AEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	CheckCombatTarget();
-	CheckPatrolTarget();
+	if (EnemyState > EEnemyState::EES_Patrolling)
+	{
+		CheckCombatTarget();
+	}
+	else
+	{
+		CheckPatrolTarget();
+	}
 }
 
 void AEnemy::CheckPatrolTarget()
